57984b613701a96513c97dcd82a93edb4c02c9ee
Glen Baker <iepathos@gmail.com>
2015-04-08 06:54:49 -0400

    Updated

--- src/main/java/enslave/Enslave.java
+++ src/main/java/enslave/Enslave.java
@@ -23,7 +23,7 @@ import enslave.network.handler.EnslavedVillagerSyncHandler;
 public class Enslave {
 	public static final String MODID = "enslave";
 	public static final String NAME = "Enslave";
-	public static final String VERSION = "0.1";
+	public static final String VERSION = "0.4";
 	public static final String CLIENTSIDE = "enslave.client.ClientProxy";
     public static final String SERVERSIDE = "enslave.CommonProxy";
     
